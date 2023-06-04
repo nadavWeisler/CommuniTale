c4a4803791edfdf023daf8c2babdb9d432a2bcdd
Nadav183 <nadav183@gmail.com>
2023-06-04 10:59:40 +0300

    Fixing issue with title parsing

--- BookGenerator/TextGenerator.py
+++ BookGenerator/TextGenerator.py
@@ -22,7 +22,8 @@ class TextGenerator:
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",  # model types: gpt-3.5-turbo, gpt-4-0314, gpt-4, gpt-3.5-turbo-0301
             messages=[{"role": "system", "content": "you are a childerns book writer you write stories up to 70 words"}]
-            + messages + [{"role": "user", "content": "Please keep title length under 22 characters long"}],
+            + messages + [{"role": "system", "content": "Please keep title length under 22 characters long"},
+                          {"role": "system", "content": "Please format the title as 'Title:\"<title>\"'"}],
             temperature=1.3,
             max_tokens=1000,
             n=n
